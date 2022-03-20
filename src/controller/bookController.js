bf626d785bef5a08aed6b14b92aad79fcba4d20d
Alisson Amaral <aalissonamaral98@gmail.com>
2022-03-20 00:35:21 -0300

    add association between schemas

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -2,18 +2,26 @@ import books from "../models/Book.js";
 
 class BookController {
   static getBookList = (req, res) => {
-    books.find((error, books) => {
-      res.status(200).json(books);
-    });
+    books
+      .find()
+      .populate("author")
+      .exec((error, books) => {
+        res.status(200).json(books);
+      });
   };
 
   static getBookById = (req, res) => {
     const id = req.params.id;
-    books.findById(id, (error, books) => {
-      if (error)
-        res.status(404).send({ message: `${error.message} - book not found` });
-      else res.status(200).json(books);
-    });
+    books
+      .findById(id)
+      .populate("author", "name")
+      .exec((error, books) => {
+        if (error)
+          res
+            .status(404)
+            .send({ message: `${error.message} - book not found` });
+        else res.status(200).json(books);
+      });
   };
 
   static createBook = (req, res) => {
